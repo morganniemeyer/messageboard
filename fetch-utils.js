6f9e10fd0f21e6961479ad4ea21bda0e5f4b753e
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-10-05 19:15:27 -0500

    set up planning docs

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,8 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://sokdbdmizbnabxtgwugf.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNva2RiZG1pemJuYWJ4dGd3dWdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ4MzEzNzAsImV4cCI6MTk4MDQwNzM3MH0.P1lyvBO99WR6qLAnNFo3UWYALquWv5tZbeEtY-869V4';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+/*SUPABASE table name = message*/
 
 /* Auth related functions */
 
